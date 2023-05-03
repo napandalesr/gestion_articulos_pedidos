3c89a29f7d9071870f4ac114ab8c06c8a7e50130
Neider Pandales <napandales@indracompany.com>
2023-05-03 11:34:52 -0500

    feature: Se agrega typescript, eslint, prettier y su configuración

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Layout from './Representation/Containers/Layout';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <Layout />
