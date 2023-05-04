162481c84d49323ba378964a888a97511dbf3b3f
Neider Pandales <napandales@indracompany.com>
2023-05-04 06:36:05 -0500

    feature: Se crea el enrutador, el server y se inicia la maquetación

--- src/Representation/Containers/Layout/index.tsx
+++ src/Representation/Containers/Layout/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "../Components/Header";
+import Loading from "../Components/Loading";
+import Router from "../../Routes";
 
 export const Layout: React.FC = () => {
-  return <>Layout</>;
+  return <>
+  <Header/>
+  <Suspense fallback={<Loading/>}>
+    <BrowserRouter>
+      <main><Router/></main>
+    </BrowserRouter>
+  </Suspense>
+  <footer></footer>
+  </>;
 };
 
 export default Layout;
