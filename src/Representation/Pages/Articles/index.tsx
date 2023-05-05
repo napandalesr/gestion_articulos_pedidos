915f4f3a4c49c4241943bb5adc026216c2c94cc0
Neider Pandales <napandales@indracompany.com>
2023-05-05 06:51:41 -0500

    feature: Se crea el formulario para los artítulos con sus tests funcionando

--- src/Representation/Pages/Articles/index.tsx
+++ src/Representation/Pages/Articles/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import ArticlesForm from "../../Containers/ArticlesForm";
+
 export const Articles: React.FC = () => {
-  return <>Articles</>;
+  return <section className="articulos p-3">
+  <h2 className="p-3">Articulos</h2>
+  <hr />
+  <ArticlesForm/>
+  </section>;
 };
 
 export default Articles;
